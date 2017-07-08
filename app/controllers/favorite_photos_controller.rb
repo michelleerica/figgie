581aed3dcb77762196e81216bd967936abdce6e8
Michelle Erica <michelle.erica.lo@gmail.com>
2017-07-08 12:31:40 +1000

    fixed favourites - un like

--- app/controllers/favorite_photos_controller.rb
+++ app/controllers/favorite_photos_controller.rb
@@ -2,16 +2,34 @@ class FavoritePhotosController < ApplicationController
   before_action :set_photo
 
   def create
-    if Favorite.create(favorited: @photo, user: @current_user)
-      redirect_to @photo, notice: 'Photo has been favorited'
-    else
-      redirect_to photo_path, alert: 'Something went wrong...*sad panda*'
+
+    # if Favorite.create(favorited: @photo, user: @current_user)
+    #   redirect_to @photo
+    # else
+    #   redirect_to photo_path
+    # end
+    #
+    # raise 'hell'
+
+    @favorites = Favorite.all
+    if (@favorites.where favorited_id: @photo.id).present?
+      (@favorites.where favorited_id: @photo.id).first.destroy
+      redirect_to photos_path
+
+    elsif
+      Favorite.create(favorited: @photo, user: @current_user)
+        redirect_to @photo
+
     end
   end
 
   def destroy
-    Favorite.where(favorited_id: @photo.id, user_id: @current_user.id).first.destroy
-    redirect_to photo_path, notice: 'Photo is no longer in favorites'
+    # Favorite.where(favorited_id: @photo.id, user_id: @current_user.id).first.destroy
+    # redirect_to @photo
+    Favorite.destroy(favorited: @photo, user: @current_user)
+      redirect_to @photo
+    redirect_to photos_path
+
   end
 
   private
